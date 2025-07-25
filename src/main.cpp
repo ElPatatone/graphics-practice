1b3653dc5d089cacce9d4ba0bf63000be12ca7d9
ElPatatone <rahman.hm2002@gmail.com>
2025-07-25 23:23:48 +0100

    feat: getting that window drawn finally

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,34 @@
 #include <GLFW/glfw3.h>
 int main (int argc, char *argv[]) {
     std::cout << "hello world" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create the GLFW window" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    int result = glfwWindowShouldClose(window);
+    std::cout << "Window has been closed" << std::endl;
+    std::cout << "value: " << result << std::endl;
+
+    glfwTerminate();
     return 0;
 }
